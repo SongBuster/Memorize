36347b1c7e2615a52c9295952f73fdbd2eadc39f
SongBuster <songbuster@gmail.com>
2024-03-04 11:45:38 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Salvador Colomina Domenech on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
