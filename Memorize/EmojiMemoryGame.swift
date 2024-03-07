d30afdd960e43f20c5b9272c028952c511c3c0d6
SongBuster <songbuster@gmail.com>
2024-03-07 12:09:15 +0100

    End Lecture 5

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,38 @@
 //  Created by Salvador Colomina Domenech on 6/3/24.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame : ObservableObject {
+    
+    private static let emojis = ["👻","🎃","😈","🕷️","🤣","👎🏼","🤷‍♂️","🥶","👷🏻","🐅","🥐","🥨"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 8) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+     
+    @Published private var model = createMemoryGame()
+    
+    var cards : Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    
+    func shuffle() {
+        model.shuffle()
+        
+    }
+    
+    func choose(_ card : MemoryGame<String>.Card) {        
+        model.choose(card)
+    }
+    
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -8,26 +8,30 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-    var viewModel : EmojiMemoryGame
-    
-    let emojis = ["👻","🎃","😈","🕷️","🤣","👎🏼","🤷‍♂️","🥶","👷🏻","🐅","🥐","🥨"]
-    
+    @ObservedObject  var viewModel : EmojiMemoryGame
     
     var body: some View {
-        
-        ScrollView {
-            cards
+        VStack {
+            ScrollView {
+                cards
+                    .animation(.default, value: viewModel.cards)
+            }
+            Button("Shuffle") {
+                viewModel.shuffle()
+            }
         }
         .padding()
-    }
-    
-   
+    }       
     
     var cards : some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(emojis.indices, id:\.self) { index in
-                CardView(content: emojis[index])
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
+            ForEach(viewModel.cards) { card in
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
             
         }
@@ -36,8 +40,12 @@ struct EmojiMemoryGameView: View {
 }
 
 struct CardView: View {
-    let content : String
-    @State var isFaceUp = true
+    
+    let card : MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
     
     var body: some View {
         ZStack {
@@ -46,18 +54,19 @@ struct CardView: View {
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
             }
-            .opacity(isFaceUp ? 1 : 0)
-            base.fill().opacity(isFaceUp ? 0 : 1)
+            .opacity(card.isFaceUp ? 1 : 0)
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
         }
-        .onTapGesture {
-            isFaceUp.toggle()
-         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
 #Preview {
-    EmojiMemoryGameView()
+    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
 }
  
